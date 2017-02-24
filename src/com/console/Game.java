ae5b8e109a3563f696dad962fc3b13b0870d60d2
kamontat <kamontat_c@hotmail.com>
2017-02-25 01:08:39 +0700

    refactor some naming, add optional inputStream to input value to play the game

--- src/com/console/Game.java
+++ src/com/console/Game.java
@@ -1,32 +1,43 @@
-package TicTacToe.console;
+package com.console;
 
-import TicTacToe.board.Board;
-import TicTacToe.player.Player;
-import TicTacToe.player.Position;
+import com.board.Board;
+import com.player.Player;
+import com.player.Position;
 
+import java.io.InputStream;
 import java.util.*;
 
 /**
  * Created by bubblebitoey on 2/23/2017 AD.
  */
 public class Game implements Runnable {
+	public static final InputStream DEFAULT_INPUT_STREAM = System.in;
+	
+	private Scanner input;
+	
 	private Board board;
 	private int currentPlayer;
 	private Player[] players;
 	
 	public Game(Board board, Player... players) {
+		input = new Scanner(DEFAULT_INPUT_STREAM);
+		this.board = board;
+		this.players = players;
+	}
+	
+	public Game(InputStream s, Board board, Player... players) {
+		input = new Scanner(s);
+		
 		this.board = board;
 		this.players = players;
 	}
 	
 	private Position input() {
-		Scanner input = new Scanner(System.in);
-		
-		System.out.println();
 		System.out.println(String.format("Player %s(%s)'s turn.", getCurrentPlayer(), getCurrentPlayer().getSymbol()));
 		try {
 			System.out.print("Please select row: ");
 			int row = input.nextInt();
+			
 			System.out.print("Please select column: ");
 			int col = input.nextInt();
 			
@@ -50,21 +61,21 @@ public class Game implements Runnable {
 	@Override
 	public void run() {
 		System.out.println(board);
-		while (board.state == Board.Condition.PLAYING) {
+		while (board.state == Board.State.PLAYING) {
 			Position p = input();
 			if (p == null) System.out.println("Accepted only number");
 			boolean isSuccess = board.insert(getCurrentPlayer(), p);
 			if (isSuccess) {
 				System.out.println(board);
-				if (board.state == Board.Condition.PLAYING) swapPlayer();
+				if (board.state == Board.State.PLAYING) swapPlayer();
 			} else {
 				System.out.println("Invalid row or column");
 			}
 		}
 		
-		if (board.state == Board.Condition.DRAW) {
+		if (board.state == Board.State.DRAW) {
 			System.out.println("Draw.");
-		} else if (board.state == Board.Condition.WIN) {
+		} else if (board.state == Board.State.WIN) {
 			System.out.println(board.winner + " winner.");
 		}
 	}

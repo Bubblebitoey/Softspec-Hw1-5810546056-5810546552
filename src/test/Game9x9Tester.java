55290c603e0fb04a79546c090275bf766c0b0819
kamontat <kamontat_c@hotmail.com>
2017-02-25 02:17:06 +0700

    add column tester and column text file

---
+++ src/test/Game9x9Tester.java
@@ -0,0 +1,75 @@
+package test;
+
+import com.board.Board;
+import com.board.shape.Square;
+import com.console.OXGame;
+import com.player.Player;
+import com.player.Position;
+import com.player.Symbol;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.*;
+
+/**
+ * @author kamontat
+ * @version 1.0
+ * @since Fri 24/Feb/2017 - 11:39 PM
+ */
+public class Game9x9Tester {
+	private final String folder = "testfile/_9x9/";
+	
+	private Board b;
+	
+	private Player p1 = new Player("p1", Symbol.O);
+	private Player p2 = new Player("p2", Symbol.X);
+	
+	private InputStream reader(String fileName) {
+		return Game9x9Tester.class.getResourceAsStream(folder + "/" + fileName);
+	}
+	
+	@Before
+	public void setBoard() {
+		b = new Board(Square.getDefaultSize());
+		System.out.println("setting new board");
+	}
+	
+	@Test
+	public void rowTester() throws IOException {
+		final String testingFile = "row.txt";
+		InputStream s = reader(testingFile);
+		
+		OXGame g = new OXGame(s, b, p1, p2);
+		try {
+			g.run();
+		} catch (NoSuchElementException e) {
+			Assert.fail("Game Must End when read file done");
+		}
+		Assert.assertEquals(p2.getSymbol(), b.getSymbol(new Position(1, 9)));
+		Assert.assertEquals(p2.getSymbol(), b.getSymbol(new Position(1, 1)));
+		
+		Assert.assertTrue(b.winner.equals(p2));
+	}
+	
+	@Test
+	public void colTester() throws IOException {
+		final String testingFile = "col.txt";
+		InputStream s = reader(testingFile);
+		
+		OXGame g = new OXGame(s, b, p1, p2);
+		try {
+			g.run();
+		} catch (NoSuchElementException e) {
+			Assert.fail("Game Must End when read file done");
+		}
+		Symbol getSymbol = b.getSymbol(new Position(1, 2));
+		
+		Assert.assertEquals(p2.getSymbol(), getSymbol);
+		Assert.assertFalse(b.isBoardFull());
+		
+		Assert.assertTrue(b.winner.equals(p1));
+	}
+}

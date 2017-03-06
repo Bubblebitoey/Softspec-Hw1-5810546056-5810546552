9a2d430d76840c1b99c9e94dba623ee69167c22f
kamontat <kamontat_c@hotmail.com>
2017-03-07 00:26:28 +0700

    add new data-structure (Pair)

---
+++ src/com/model/implementation/Pair.java
@@ -0,0 +1,66 @@
+package com.model.implementation;
+
+/**
+ * @author kamontat
+ * @version 1.0
+ * @since Sun 05/Mar/2017 - 3:29 AM
+ */
+public class Pair<K, V> {
+	private K key;
+	private V value;
+	
+	/**
+	 * easy create the pair.
+	 *
+	 * @param key
+	 * 		key or first element.
+	 * @param value
+	 * 		value or second element.
+	 * @param <K>
+	 * 		key type/class.
+	 * @param <V>
+	 * 		value type/class.
+	 * @return Pair that contains key and value.
+	 */
+	public static <K, V> Pair<K, V> create(K key, V value) {
+		return new Pair<K, V>(key, value);
+	}
+	
+	public Pair() {
+		key = null;
+		value = null;
+	}
+	
+	public Pair(K key, V value) {
+		this.key = key;
+		this.value = value;
+	}
+	
+	public K getKey() {
+		return key;
+	}
+	
+	public void setKey(K key) {
+		this.key = key;
+	}
+	
+	public V getValue() {
+		return value;
+	}
+	
+	public void setValue(V value) {
+		this.value = value;
+	}
+	
+	public boolean isKeyContains(K key) {
+		return this.key.equals(key);
+	}
+	
+	public boolean isValueContains(V value) {
+		return this.value.equals(value);
+	}
+	
+	public boolean isContains(Object obj) {
+		return obj.equals(key) || obj.equals(value);
+	}
+}
